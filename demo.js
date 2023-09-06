0e8f6c7fc13223b28e1c71d907fd7fa6bec53aef
sayam9901 <sayamkhan9560621912@gmail.com>
2023-09-06 23:46:40 +0530

    my first commit

---
+++ demo.js
@@ -0,0 +1,20 @@
+// console.dir(document)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.links)
+// console.log(document.images)
+
+//getelementbtid
+
+// console.log(document.getElementById("main-header"))
+// var headerTitle = document.getElementById("header-title")
+// var Myheader = document.getElementById("main-header")
+var item = document.getElementById('items')
+item.style.fontWeight = "bold"
+item.style.color = 'green'
+// console.log(headerTitle)
+// console.log(Myheader)
+// headerTitle.textContent = "Hello"
+// headerTitle.innerHTML = "GOOD BYE"
+// Myheader.style.borderBottom= "solid 3px #000"
